98fb1f211baa9c13c9d964dae44da68275d9986f
Aisser24 <jonathanguger@gmail.com>
2022-10-06 15:44:03 +0200

    init

---
+++ index.js
@@ -0,0 +1,95 @@
+const aEl = document.querySelector(".typA-comp")
+const bEl = document.querySelector(".typB-comp")
+const cEl = document.querySelector(".typC-comp")
+const ccEl = document.querySelector(".typCC-comp")
+const dEl = document.querySelector(".typD-comp")
+const eEl = document.querySelector(".typE-comp")
+const fEl = document.querySelector(".typF-comp")
+const efEl = document.querySelector(".typEF-comp")
+const gEl = document.querySelector(".typG-comp")
+const hEl = document.querySelector(".typH-comp")
+const iEl = document.querySelector(".typI-comp")
+const jEl = document.querySelector(".typJ-comp")
+const kEl = document.querySelector(".typK-comp")
+const lEl = document.querySelector(".typL-comp")
+const mEl = document.querySelector(".typM-comp")
+const la = document.querySelector(".la")
+const lb = document.querySelector(".lb")
+const lc = document.querySelector(".lc")
+const lcc = document.querySelector(".lcc")
+const ld = document.querySelector(".ld")
+const le = document.querySelector(".le")
+const lf = document.querySelector(".lf")
+const lef = document.querySelector(".lef")
+const lg = document.querySelector(".lg")
+const lh = document.querySelector(".lh")
+const li = document.querySelector(".li")
+const lj = document.querySelector(".lj")
+const lk = document.querySelector(".lk")
+const ll = document.querySelector(".ll")
+const lm = document.querySelector(".lm")
+
+
+function open(sub) {
+    sub.classList.toggle("hidden")
+}
+
+aEl.addEventListener("click", () => {
+    open(la)
+})
+
+bEl.addEventListener("click", () => {
+    open(lb)
+})
+
+cEl.addEventListener("click", () => {
+    open(lc)
+})
+
+ccEl.addEventListener("click", () => {
+    open(lcc)
+})
+
+dEl.addEventListener("click", () => {
+    open(ld)
+})
+
+eEl.addEventListener("click", () => {
+    open(le)
+})
+
+fEl.addEventListener("click", () => {
+    open(lf)
+})
+
+efEl.addEventListener("click", () => {
+    open(lef)
+})
+
+gEl.addEventListener("click", () => {
+    open(lg)
+})
+
+hEl.addEventListener("click", () => {
+    open(lh)
+})
+
+iEl.addEventListener("click", () => {
+    open(li)
+})
+
+jEl.addEventListener("click", () => {
+    open(lj)
+})
+
+kEl.addEventListener("click", () => {
+    open(lk)
+})
+
+lEl.addEventListener("click", () => {
+    open(ll)
+})
+
+mEl.addEventListener("click", () => {
+    open(lm)
+})
